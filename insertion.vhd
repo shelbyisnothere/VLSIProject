3e7aa6dcc5944aa8a486d0ca5fe38b21a1487e2c
chefwang24 <98505731+chefwang24@users.noreply.github.com>
2022-01-31 23:52:06 -0600

    Update insertion.vhd

--- insertion.vhd
+++ insertion.vhd
@@ -1,46 +1,88 @@
 module insertion(input clk, 
                  input reset,
                  input quarter_in,
-                 input dollar_in,
-                 input t_complete,
+                 //input dollar_in,
+                 input select1,
+		 input select2,
                  output i[2:0], 
-                 output Q,
-                 output D);
+		 output quarter);
 	input clk;
 	input reset;
 	input quarter_in;
-	input dollar_in;
-	input t_complete;
+	//input dollar_in;
+	//input t_complete;
   output i[2:0];
-	output Q;
-	output D;
+	output s1;
+	output s2;
   
   reg [3:0] state;
 
   // State encodings
   parameter [3:0]
-    I0    	= 4'B0000,
-    I1 		= 4'b0001,
-    I2   	= 4'b0010;
-  
+    I0    	= 3'b000,
+    I1 		= 3'b001,
+    I2   	= 3'b010;
+    I3   	= 3'b011;
+    I4   	= 3'b100;
+    I5   	= 3'b101;
+    I6   	= 3'b110;
   // State transitions
   always @(posedge clk) begin
     case (state)
-      I0:
-        if (quarter_in) begin
+      I0: 
+	i <= 2'b00;
+	s1 <= 0;
+ 	s2 <= 0;
+	quarter <= 0;
+        if (quarter_in) 
           state <= I1;
-          i <= 3'B000;
-          Q <= 0;
-          D <= 0;
-        end elseif (dollar_in) begin
-          
-        end elseif (t_complete) begin
-          
-        end
+        end if
       I1:
-
+	i <= 2'b01;
+	s1 <= 0;
+ 	s2 <= 0;
+	quarter <= 0;
+	if (quarter_in) 
+          state <= I2;
+        end if
       I2:
-
+	i <= 2'b10;
+	s1 <= 0;
+ 	s2 <= 0;
+	quarter <= 0;
+	if (quarter_in) 
+          state <= I3;
+	else if (select1)
+	  state <= I5;
+        end if
+      I3:
+	i <= 2'b11;
+	s1 <= 0;
+ 	s2 <= 0;
+	quarter <= 0;
+	if (select1) 
+	  state <= I6;
+	if (select2)
+	  state <= I4;	
+	end if
+	I4:
+	i <= 2'b11;
+	s1 <= 0;
+ 	s2 <= 1;
+`	quarter <= 0;
+	  state <= I0;
+	I5:
+	i <= 2'b10;
+	s1 <= 1;
+ 	s2 <= 0;
+`	quarter <= 0;
+	  state <= I0;		  
+	I6:
+	i <= 2'b11;
+	s1 <= 0;
+ 	s2 <= 1;
+`	quarter <= 1;
+	  state <= I0;			  
       default:
         state <= I0;
     endcase
