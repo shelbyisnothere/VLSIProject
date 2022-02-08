53b40d93db6c3f9f92d2e1a79db1ea319b60975e
Shleby <47256732+shelbyisnothere@users.noreply.github.com>
2022-02-07 21:05:14 -0600

    Update vending_machine.v

--- vending_machine.v
+++ vending_machine.v
@@ -1,5 +1,5 @@
 /*
- * Filename: vending_controller_fsm.v
+ * Filename: vending_controller.v
  * Authors: Anna Nguyen, David Wang, Shelby King
  * Date: 2/8/2022
  * Course: Introduction to VLSI Design EE4325.001 Spring 2022
@@ -30,7 +30,7 @@ module vending_controller(clk, reset, quarter_in, select1, select2,
   output reg product2;		// Product 2; costs $0.75
   output reg quarter_out;	// A quarter has been dispensed by the machine
   
-  typedef enum logic [2:0] {S0, S1, S2, S3, S4, S5, S6} statetype;
+  typedef enum logic [2:0] {S0, S1, S2, S3, S4, S5, S6, S7} statetype;
   statetype state, next_state;
   
   always @(posedge clk)
@@ -49,26 +49,31 @@ module vending_controller(clk, reset, quarter_in, select1, select2,
           if(quarter_in) next_state = S2;
         S2:
           if(quarter_in) next_state = S3;
+          else if (select1 && select2) next_state = S2;
           else if(select1) next_state = S5;
-        S3: 
-          if(select2) next_state = S4;
+        S3:
+          if(quarter_in) next_state = S7;
+          else if (select1 && select2) next_state = S3;
           else if(select1) next_state = S6;
+          else if(select2) next_state = S4;
         S4:	
           next_state = S0;
         S5: 
           next_state = S0;
         S6: 
           next_state = S0;
+        S7:
+          next_state = S3;
         default: 
           next_state = S0;
       endcase
     end
   
   // Output logic
-  assign i = {(state == S2 | state == S3 | state == S4 | state == S5 | state == S6),
-              (state == S1 | state == S3 | state == S4 | state == S6)};
+  assign i = {(state == S2 | state == S3 | state == S4 | state == S5 | state == S6 | state == S7),
+              (state == S1 | state == S3 | state == S4 | state == S6 | state == S7)};
   assign product1 = (state == S5 | state == S6);
   assign product2 = (state == S4);
-  assign quarter_out = (state == S6);
+  assign quarter_out = (state == S6 | state == S7);
   
 endmodule // vending_controller
