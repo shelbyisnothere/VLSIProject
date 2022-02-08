0d579e3575f2045172ae767db162f8ce1dd435d3
Shleby <47256732+shelbyisnothere@users.noreply.github.com>
2022-02-07 20:35:16 -0600

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,12 +1,19 @@
 /*
- * Filename: testbench.vhd
+ * Filename: testbench.v
  * Authors: Anna Nguyen, David Wang, Shelby King
  * Date: 2/8/2022
  * Course: Introduction to VLSI Design EE4325.001 Spring 2022
  * Description:
- *	Test bench for vending machine FSM.
+ *	Test bench for vending machine FSM. Inspiration taken from project
+ *		resourses, as well as the course text book - CMOS VLSI Design,
+ *		Weste and Harris. 
+ *	
  */
 
+// vending_controller_testbench
+//		Testbench for the vending machine project. Reads from an input
+//		file using the readmemb verilog standard function. Cases defined
+//		and explained are located in the test_input.tv file.
 module vending_controller_testbench;
   logic clk, reset, quarter_in, select1, select2;
   logic [1:0] i;
@@ -19,7 +26,6 @@ module vending_controller_testbench;
   logic [31:0] vector_count, errors;
   logic [7:0] test_vectors[10000:0];
   logic [1:0] i_expected;
-  logic i1, i0;
   logic p1_expected, p2_expected, qout_expected;
   
   // Instantiate FSM
@@ -53,19 +59,19 @@ module vending_controller_testbench;
   always @(posedge clk)
     begin
       #1;
-      {quarter_in, select1, select2, i1, i0, p1_expected, p2_expected, qout_expected} = test_vectors[vector_count];
+      {quarter_in, select1, select2, i_expected, p1_expected, p2_expected, qout_expected} = test_vectors[vector_count];
     end
   
   // Check results on descending edge of clock
   always @(negedge clk)
     if(~reset)
       begin
-        if ((i[1] !== i1) | (i[0] !== i0))
+        if ((i[1] !== i_expected[1]) | (i[0] !== i_expected[0]))
           begin
             $display("ERROR: Incorrect value for i");
             $display("		inputs = %b", {quarter_in, select1, select2});
             $display("		outputs = %b", {i[1], i[0], quarter_out, product1, product2});
-            $display("		expected outputs = %b", {i1, i0, qout_expected, p1_expected, p2_expected});
+            $display("		expected outputs = %b", {i_expected[1], i_expected[0], qout_expected, p1_expected, p2_expected});
             errors = errors + 1;
           end
         if ((product1 !== p1_expected) | (product2 !== p2_expected))
@@ -73,7 +79,7 @@ module vending_controller_testbench;
             $display("ERROR: Incorrect value for a product");
             $display("		inputs = %b", {quarter_in, select1, select2});
             $display("		outputs = %b", {i[1], i[0], quarter_out, product1, product2});
-            $display("		expected outputs = %b", {i1, i0, qout_expected, p1_expected, p2_expected});
+            $display("		expected outputs = %b", {i_expected[1], i_expected[0], qout_expected, p1_expected, p2_expected});
             errors = errors + 1;
           end
         if (quarter_out !== qout_expected)
@@ -81,7 +87,7 @@ module vending_controller_testbench;
             $display("ERROR: Incorrect value for quarter_out");
             $display("		inputs = %b", {quarter_in, select1, select2});
             $display("		outputs = %b", {i[1], i[0], quarter_out, product1, product2});
-            $display("		expected outputs = %b", {i1, i0, qout_expected, p1_expected, p2_expected});
+            $display("		expected outputs = %b", {i_expected[1], i_expected[0], qout_expected, p1_expected, p2_expected});
             errors = errors + 1;
           end
         
